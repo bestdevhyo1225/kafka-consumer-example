921dcbcfca89dd186c9f2f1c514a86562359065b
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-15 15:18:15 +0900

    feat: streams-pipe-output 토픽을 구독하는 컨슈머 구현

--- src/main/java/com/example/kafka/ConsumerGroupValue.java
+++ src/main/java/com/example/kafka/ConsumerGroupValue.java
@@ -4,4 +4,5 @@ public abstract class ConsumerGroupValue {
     public static final String HYO = "hyo";
     public static final String HYO_MANUAL = "hyo-manual";
     public static final String HYO_PARTITION = "hyo-partition";
+    public static final String HYO_STREAMS = "hyo-streams";
 }
