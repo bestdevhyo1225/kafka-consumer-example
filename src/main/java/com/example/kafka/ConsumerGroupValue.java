932a9e40e48d4ad6ac90e217ad342a60124dccca
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-16 22:03:56 +0900

    feat: 카프카 스트림즈 - 행 분리 컨슈머 예제 추가

--- src/main/java/com/example/kafka/ConsumerGroupValue.java
+++ src/main/java/com/example/kafka/ConsumerGroupValue.java
@@ -5,4 +5,5 @@ public abstract class ConsumerGroupValue {
     public static final String HYO_MANUAL = "hyo-manual";
     public static final String HYO_PARTITION = "hyo-partition";
     public static final String HYO_STREAMS = "hyo-streams";
+    public static final String HYO_STREAMS_LINE_SPLIT = "hyo-streams-line-split";
 }
--- src/main/java/com/example/kafka/KafkaTopic.java
+++ src/main/java/com/example/kafka/KafkaTopic.java
@@ -3,4 +3,5 @@ package com.example.kafka;
 public abstract class KafkaTopic {
     public static final String HYO_TOPIC = "hyo-topic";
     public static final String STREAMS_PIPE_OUTPUT = "streams-pipe-output";
+    public static final String STREAMS_LINE_SPLIT_OUTPUT = "streams-line-split-output";
 }
